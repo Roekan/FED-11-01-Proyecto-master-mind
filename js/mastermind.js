84d97028f4b1bc4c863906bb2264bc1ededda3be
Alvaro <Roekan03@gmail.com>
2023-06-13 16:37:06 +0200

    create styles of mastermind pages

--- js/mastermind.js
+++ js/mastermind.js
@@ -7,8 +7,23 @@ let dificultad = document.getElementById("dificultad");
 let name = `${sessionStorage.getItem("nombre")}`;
 let dificult = `${sessionStorage.getItem("dificultad")}`;
 
+let nivel ='';
+switch (dificult){
+    case '1':
+        nivel="Fácil";
+        break;
+    case '2':
+        nivel="Medio";
+        break;
+    case '3':
+        nivel="Difícil";
+        break;
+    default:
+        nivel="No especificado";
+}
 
 
-nombre.innerHTML = name;
-dificultad.innerHTML = dificult;
+
+nombre.innerHTML = `<span class="texto-info-user">Jugador: </span> <span class="param-info-user">${name}</span> `;
+dificultad.innerHTML = `<span class="texto-info-user">Nivel: </span> <span class="param-info-user">${nivel}</span> `;
 
