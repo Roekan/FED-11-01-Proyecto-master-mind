2fe00df05072e03a8c72a7c9cbde3ed8fb5e7eec
Alvaro <Roekan03@gmail.com>
2023-06-16 18:08:26 +0200

    change css styles on mastermind.html

--- js/mastermind.js
+++ js/mastermind.js
@@ -13,6 +13,7 @@ let levelSession = "";
 let quantityRows = 0;
 let quantityColours = 0;
 //elemento tablero
+let boxBoard= document.getElementById("boxBoard");
 let board = document.getElementById("board");
 const quantityChips = 4;
 
@@ -37,10 +38,10 @@ const checkColours = () => {
   });
 
   console.log(coloursUser);
-
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////
   //comprobar colores con objeto colorBoss y pintar las clues
   //cambiar de activeRow
-
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////
   // Elimina el evento en la fila
   eraseEventColours();
   // Elimina el icono del check
@@ -62,11 +63,11 @@ const checkColours = () => {
   }
 };
 
-//Funcion que genera un color del arrayde colores aleatorio
+//FUNCION que genera un color del arrayde colores aleatorio
 const getRandomColor = (arrColours) =>
   arrColours[Math.floor(Math.random() * arrColours.length)];
 
-//Funcion Convertir color RGB a Hexadecimal
+//FUNCION Convertir color RGB a Hexadecimal
 function rgbToHex(col) {
   if (col.charAt(0) == "r") {
     col = col.replace("rgb(", "").replace(")", "").split(",");
@@ -81,8 +82,9 @@ function rgbToHex(col) {
   }
 }
 
-//Funcion  cambiar los colores al hacer click
+//FUNCION  cambiar los colores al hacer click
 function eventListenerChangeColor(e) {
+  // Si no hay color ponme el primero
   if (!e.target.style.backgroundColor) {
     e.target.style.backgroundColor = arrayColours[0];
   } else {
@@ -91,10 +93,13 @@ function eventListenerChangeColor(e) {
     );
     e.target.style.backgroundColor =
       indexColor == quantityColours - 1
-        ? arrayColours[0]
-        : arrayColours[indexColor + 1];
+        ? //Si hay color y es el ultimo del array, ponme el primer color
+          arrayColours[0]
+        : //Si hay color pero no es el ultimo ponme el siguiente color del array
+          arrayColours[indexColor + 1];
   }
 }
+
 //FUNCION Añadir funcionalidad evento onclick al la fila para que cambie de color
 const changeColours = () => {
   Array.from(
@@ -116,29 +121,32 @@ const eraseEventColours = () => {
 };
 /*/////////////////END FUNCIONES//////////////////// */
 
-///////////////////////////////Código
+///////////////////////////////CÓDIGO//////////////////////////////////
 
+//Genero la jugada del BOSS
 for (let i = 1; i <= quantityChips; i++) {
   coloursBoss[i] = getRandomColor(arrayColours);
 }
 console.log(`Colores de la máquina: `, coloursBoss);
-//let arrayColours=['red','blue','yellow','green','purple','orange']
-
+//Genero la cantidad de filas y colores segun la dificultad que recibo de settings
 switch (levelSessionType) {
   case "1":
     levelSession = "Fácil";
     quantityRows = 10;
     quantityColours = 4;
+    boxBoard.classList.add('boxBoard-easy')
     break;
   case "2":
     levelSession = "Medio";
     quantityRows = 8;
     quantityColours = 5;
+    boxBoard.classList.add('boxBoard-medium')
     break;
   case "3":
     levelSession = "Difícil";
     quantityRows = 6;
     quantityColours = 6;
+    boxBoard.classList.add('boxBoard-hard')
     break;
   default:
     levelSession = "No especificado";
@@ -148,6 +156,7 @@ activeRow = quantityRows;
 //Pinta Nombre del jugador y dificultad de la partida
 name.innerHTML = `<span class="texto-info-user">Jugador: </span> <span class="param-info-user">${nameSession}</span> `;
 level.innerHTML = `<span class="texto-info-user">Nivel: </span> <span class="param-info-user">${levelSession}</span> `;
+
 //Pinta las filas segun la dificultad seleccionada
 for (let i = 1; i <= quantityRows; i++) {
   board.innerHTML += `
@@ -165,9 +174,9 @@ for (let i = 1; i <= quantityRows; i++) {
             <div class="chip chip-vacio"></div>
         </div>
         <div class="col row-board d-flex">
-            <button onclick="checkColours()" id=check-${i} class="border border-0 bg-dark p-1 ${
+            <button onclick="checkColours()" id=check-${i} class="border border-0 p-1 align-items-center justify-content-center ${
     i == quantityRows ? "d-flex" : "d-none"
-  }"><svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24"><path fill="#fff" d="m10.6 16.2l7.05-7.05l-1.4-1.4l-5.65 5.65l-2.85-2.85l-1.4 1.4l4.25 4.25ZM5 21q-.825 0-1.413-.588T3 19V5q0-.825.588-1.413T5 3h14q.825 0 1.413.588T21 5v14q0 .825-.588 1.413T19 21H5Zm0-2h14V5H5v14ZM5 5v14V5Z"></path></svg></button>
+  } box-check"><svg class="check-img" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24"><path fill="#fff" d="m10.6 16.2l7.05-7.05l-1.4-1.4l-5.65 5.65l-2.85-2.85l-1.4 1.4l4.25 4.25ZM5 21q-.825 0-1.413-.588T3 19V5q0-.825.588-1.413T5 3h14q.825 0 1.413.588T21 5v14q0 .825-.588 1.413T19 21H5Zm0-2h14V5H5v14ZM5 5v14V5Z"></path></svg></button>
         </div>
     </div> `;
 }
