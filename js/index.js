4c16cc2ee0ecc8545ae40fb3b3de83b8355b3708
Alvaro <Roekan03@gmail.com>
2023-06-22 16:45:03 +0200

    fix animation index

--- js/index.js
+++ js/index.js
@@ -12,12 +12,14 @@ const animationTitle=(url) =>{
 
     window.setTimeout(() =>  {
         window.location.href=url
-
     },1000); 
-    
-    gameTitle.classList.add('game-title-animation-load')
-    gameTitle.classList.remove('game-title-animation-click')
-    btnStartGame.classList.remove('buttons-title-animation-click')
-    btnStartInstructions.classList.remove('buttons-title-animation-click')
+   
+    window.setTimeout(() =>  {
+        gameTitle.classList.add('game-title-animation-load')
+        gameTitle.classList.remove('game-title-animation-click')
+        btnStartGame.classList.remove('buttons-title-animation-click')
+        btnStartInstructions.classList.remove('buttons-title-animation-click')
+
+    },2000); 
 
 }
