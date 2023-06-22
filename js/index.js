102e44dd3e3be64bbe3ff8064d1c8328120a7c3b
Alvaro <Roekan03@gmail.com>
2023-06-22 17:12:14 +0200

    fix animation index

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 let gameTitle=document.getElementById('game-title')
 let btnStartGame=document.getElementById('btn-start-game')
 let btnStartInstructions=document.getElementById('btn-start-instructions')
+let divBtnStartGame=document.getElementById('box-btn-start-game')
 let divBtnStartInstructions=document.getElementById('box-btn-start-instructions')
 
 const animationTitle=(url) =>{
@@ -10,6 +11,8 @@ const animationTitle=(url) =>{
     gameTitle.classList.add('game-title-animation-click')
 
     divBtnStartInstructions.classList.remove('box-title')
+    divBtnStartGame.classList.remove('box-title')
+
     btnStartGame.classList.add('buttons-title-animation-click')
     btnStartInstructions.classList.add('buttons-title-animation-click')
     
@@ -22,6 +25,8 @@ const animationTitle=(url) =>{
         gameTitle.classList.remove('game-title-animation-click')
 
         divBtnStartInstructions.classList.add('box-title')
+        divBtnStartGame.classList.add('box-title')
+
         btnStartGame.classList.remove('buttons-title-animation-click')
         btnStartInstructions.classList.remove('buttons-title-animation-click')
 
